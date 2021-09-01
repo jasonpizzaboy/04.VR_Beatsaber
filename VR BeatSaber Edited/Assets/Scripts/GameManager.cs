ff54928dadc77bc316078a3dbefdffbba910d2af
JasonKim <saurus234@gmail.com>
2021-09-01 10:52:34 +0900

    updated 0901

---
+++ VR BeatSaber Edited/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
